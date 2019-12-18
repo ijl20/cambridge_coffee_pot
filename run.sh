e545016d5c7617507af1af8460dda803c724f107
Ian Lewis <ijl20@cam.ac.uk>
2019-12-18 14:31:56 +0000

    run.sh includes data collection version

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ echo $(date) '[info] cambridge_coffee_pot/run.sh starting...'
 cd /home/pi/cambridge_coffee_pot/code
 
 # echo $(date) '[warning] cambridge_coffee_pot/run.sh disabled'
+
+# Data readings collection version:
+#python3 main.py config/sensor_debug.json
+
+# Normal version
 python3 main.py
 
 echo $(date) '[info] cambridge_coffee_pot/run.sh completed'
