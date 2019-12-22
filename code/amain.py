a186ae2339ea83b1b8f37725d8a05594efd745ee
Ian Lewis <ijl20@cam.ac.uk>
2019-12-22 15:22:41 +0000

    initial async version with asyncio, gmqtt WIP

---
+++ code/amain.py
@@ -0,0 +1,60 @@
+import asyncio
+import os
+import signal
+import time
+
+from gmqtt.mqtt.constants import MQTTv311
+from gmqtt import Client as MQTTClient
+
+# gmqtt also compatibility with uvloop  
+#import uvloop
+#asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+STOP = asyncio.Event()
+
+def on_connect(client, flags, rc, properties):
+    print('Connected')
+    client.subscribe('TEST/#', qos=0)
+
+
+def on_message(client, topic, payload, qos, properties):
+    print('RECV MSG:', payload)
+
+
+def on_disconnect(client, packet, exc=None):
+    print('Disconnected')
+
+def on_subscribe(client, mid, qos):
+    print('SUBSCRIBED')
+
+def ask_exit(*args):
+    STOP.set()
+
+async def main(broker_host, broker_port, token):
+    client = MQTTClient("client-id")
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+    client.on_subscribe = on_subscribe
+
+    #client.set_auth_credentials(token, None)
+    await client.connect(broker_host, broker_port, version=MQTTv311)
+
+    client.publish('TEST/TIME', str(time.time()), qos=1)
+
+    await STOP.wait()
+    await client.disconnect()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    host = 'localhost'
+    token = None #os.environ.get('FLESPI_TOKEN')
+
+    loop.add_signal_handler(signal.SIGINT, ask_exit)
+    loop.add_signal_handler(signal.SIGTERM, ask_exit)
+
+    loop.run_until_complete(main(host, 1887, token))
--- code/main.py
+++ code/main.py
@@ -6,6 +6,12 @@
 
 import sys
 import time
+import asyncio
+import signal
+
+# Async MQTT
+from gmqtt.mqtt.constants import MQTTv311
+from gmqtt import Client as MQTTClient
 
 from classes.sensor import Sensor
 
@@ -15,7 +21,63 @@ from classes.config import Config
 
 from classes.sensor_utils import list_to_string
 
-VERSION = "0.60"
+VERSION = "0.80"
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# Class Comms
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+
+class Comms(object):
+
+    def __init__(self, handle_input):
+        print("Comms init()")
+
+        self.handle_input = handle_input
+
+        self.STOP = asyncio.Event()
+
+        self.client = MQTTClient("client-id")
+
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_subscribe = self.on_subscribe
+
+
+    async def begin(self, broker_host, broker_port):
+
+        #client.set_auth_credentials(token, None)
+        await self.client.connect(broker_host, broker_port, version=MQTTv311)
+
+        self.client.publish('TEST/TIME', str(time.time()), qos=1)
+
+    async def finish(self):
+
+        #await self.STOP.wait()
+        await self.client.disconnect()
+
+    def on_connect(self, client, flags, rc, properties):
+        print('Connected')
+        self.client.subscribe('TEST/#', qos=0)
+
+
+    def on_message(self, client, topic, payload, qos, properties):
+        self.handle_input(payload)
+
+
+    def on_disconnect(self, client, packet, exc=None):
+        print('Disconnected')
+
+    def on_subscribe(self, client, mid, qos):
+        print('SUBSCRIBED')
+
+    def ask_exit(self, *args):
+        self.STOP.set()
+
+def handle_input(input):
+    print("got input",input)
 
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
@@ -23,7 +85,7 @@ VERSION = "0.60"
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
 
-if __name__ == "__main__":
+async def main():
 
     print("main started with {} arguments {}".format(len(sys.argv), list_to_string(sys.argv)))
 
@@ -34,6 +96,10 @@ if __name__ == "__main__":
         config = Config()
 
     config.settings["VERSION"] = VERSION
+
+    comms = Comms(handle_input)
+
+    await comms.begin('localhost', 1887)
 
     s = Sensor(settings = config.settings)
 
@@ -60,7 +126,7 @@ if __name__ == "__main__":
 
             loop_time = time.time() - loop_start_time
             if loop_time < 0.1:
-                time.sleep(0.1 - loop_time)
+                await asyncio.sleep(0.1 - loop_time)
 
     except (KeyboardInterrupt, SystemExit):
         pass
@@ -68,3 +134,10 @@ if __name__ == "__main__":
     # Cleanup and quit
     s.finish()
 
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    #loop.add_signal_handler(signal.SIGINT, ask_exit)
+    #loop.add_signal_handler(signal.SIGTERM, ask_exit)
+
+    loop.run_until_complete(main())
