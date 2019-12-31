63fbc614a8361d81f7e2d15116bf1e0cdeb587ab
Ian Lewis <ijl20@cam.ac.uk>
2019-12-31 20:01:52 +0000

    test_hbmqtt working

--- code/test_hbmqtt.py
+++ code/test_hbmqtt.py
@@ -2,25 +2,29 @@ import logging
 import asyncio
 import time
 import random
+
 import simplejson as json
+from simplejson.errors import JSONDecodeError
 
 from hbmqtt.client import MQTTClient, ClientException
 from hbmqtt.mqtt.constants import QOS_0, QOS_1, QOS_2
 
 from classes.time_buffer import TimeBuffer
 
-UPLINK_HOST = 'mqtt://192.168.1.51'
+settings = {
+    "UPLINK_HOST": 'mqtt://192.168.1.51',
+    "SENSOR_ID": "csn-node-test"
+}
 
 async def uplink_init():
     print('uplink_init() startup')
     uplink_client = MQTTClient()
-    await uplink_client.connect(UPLINK_HOST)
-    print('uplink_init() connected {}'.format(UPLINK_HOST))
+    await uplink_client.connect(settings["UPLINK_HOST"])
+    print('uplink_init() connected {}'.format(settings["UPLINK_HOST"]))
     return uplink_client
 
 async def uplink_send(uplink_client, topic, message=None):
-    print('uplink_send() sending {} {}'.format(topic,message))
-    #topic_b = bytes(topic,'utf-8')
+    print('uplink_send() sending {}'.format(topic))
     message_b = bytes(message,'utf-8')
     tasks = [
         asyncio.ensure_future(uplink_client.publish(topic, message_b))
@@ -37,14 +41,14 @@ async def remote_event_detect(uplink_client, sample_buffer, event_buffer):
     #debug send event for every message from remote sensors
     if True:
         remote_event = {   "acp_ts": time.time(),
-                           "acp_id": "csn-node-test",
+                           "acp_id": settings["SENSOR_ID"],
                            "event_code": "COFFEE_REMOTE",
                            "event_value": value
                        }
 
         remote_msg = json.dumps(remote_event)
 
-        await uplink_send(uplink_client, "csn-node-test", remote_msg)
+        await uplink_send(uplink_client, settings["SENSOR_ID"], remote_msg)
 
 
 async def remote_sensors(uplink_client, event_buffer):
@@ -52,65 +56,82 @@ async def remote_sensors(uplink_client, event_buffer):
 
     sample_buffer = TimeBuffer(100)
 
-    sensor_client = MQTTClient('csn-node-test')
+    sensor_client = MQTTClient(settings["SENSOR_ID"])
     await sensor_client.connect('mqtt://localhost')
     await sensor_client.subscribe([
-            ('csn-node-test-a/#', QOS_0),
-            ('csn-node-test-b/#', QOS_0)
+            (settings["SENSOR_ID"]+"-a/#", QOS_0),
+            (settings["SENSOR_ID"]+"-b/#", QOS_0)
          ])
     try:
         while True:
-            message = await sensor_client.deliver_message()
-            packet = message.publish_packet
+            message_obj = await sensor_client.deliver_message()
+            packet = message_obj.publish_packet
+            topic = packet.variable_header.topic_name
 
+            print("remote_sensors() topic received {}".format(topic))
+
+            message = ""
             if packet.payload is None:
-                print("remote_sensors packet.payload=None {}".format(packet.variable_header.topic_name))
+                print("remote_sensors packet.payload=None {}".format(topic))
             elif packet.payload.data is None:
-                print("remote_sensors() packet.payload.data=None {}".format(packet.variable_header.topic_name))
+                print("remote_sensors() packet.payload.data=None {}".format(topic))
             else:
-                print("remote_sensors() {} => {}".format(packet.variable_header.topic_name,
-                                                          packet.payload.data))
+                message = packet.payload.data.decode('utf-8')
+                print("remote_sensors() {} => {}".format(topic,message))
 
-                                                          ##DEBUG BUG HERE BYTES->STRING
-            sample_buffer.put(time.time(), json.loads(packet.payload.data))
+            message_dict = {}
+            try:
+                message_dict = json.loads(message)
+            except JSONDecodeError:
+                message_dict["message"] = message
+                print("remote_sensors() json msg error: {} => {}".format(topic,message))
+
+            message_dict["topic"] = topic
+
+            sample_buffer.put(time.time(), message_dict)
+
             await remote_event_detect(uplink_client, sample_buffer, event_buffer)
 
     except ClientException as client_exception:
         print("remote_sensors() Client exception: {}".format(client_exception))
 
     await sensor_client.unsubscribe([
-        ('csn-node-test-a/#'),
-        ('csn-node-test-b/#')
+        (settings["SENSOR_ID"]+"-a/#"),
+        (settings["SENSOR_ID"]+"-b/#")
     ])
+
+    print("remote_sensors() unsubscribed")
 
     await sensor_client.disconnect()
 
+    print("remote_sensors disconnected()")
+    
 async def local_event_detect(uplink_client, sample_buffer, event_buffer):
     sample = sample_buffer.get(0)
 
     if not sample is None and sample["value"] > 99:
         watchdog_event = { "acp_ts": time.time(),
-                           "acp_id": "csn-node-test",
+                           "acp_id": settings["SENSOR_ID"],
                            "event_code": "COFFEE_WATCHDOG",
                            "event_value": sample["value"]
                          }
 
         watchdog_msg = json.dumps(watchdog_event)
 
-        await uplink_send(uplink_client, "csn-node-test", watchdog_msg)
+        await uplink_send(uplink_client, settings["SENSOR_ID"], watchdog_msg)
 
 async def local_sensor(uplink_client, event_buffer):
 
     sample_buffer = TimeBuffer(1000)
 
     startup_event = { "acp_ts": time.time(),
-                      "acp_id": "csn-node-test",
+                      "acp_id": settings["SENSOR_ID"],
                       "event_code": "COFFEE_STARTUP"
                     }
 
     startup_msg = json.dumps(startup_event)
 
-    await uplink_send(uplink_client, "csn-node-test", startup_msg)
+    await uplink_send(uplink_client, settings["SENSOR_ID"], startup_msg)
 
     quit = False
     while not quit:
