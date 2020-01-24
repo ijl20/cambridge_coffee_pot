8004e4406c7b2591cf7e1571fbbbfa8e4213b9cd
Ian Lewis <ijl20@cam.ac.uk>
2020-01-24 17:31:08 +0000

    fixed autostart as service

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo $(date) '[info] cambridge_coffee_pot/run.sh starting...'
+echo $(date) '[info] cambridge_coffee_pot/run.sh starting...' >>/home/pi/cambridge_coffee_pot.log
 
 cd /home/pi/cambridge_coffee_pot/code
+
+source /home/pi/cambridge_coffee_pot_venv/bin/activate
 
 # echo $(date) '[warning] cambridge_coffee_pot/run.sh disabled'
 
@@ -10,7 +12,7 @@ cd /home/pi/cambridge_coffee_pot/code
 #python3 main.py config/sensor_debug.json
 
 # Normal version
-python3 main.py
+python3 main.py >/home/pi/cambridge_coffee_pot_run.log 2>/home/pi/cambridge_coffee_pot_run.err
 
-echo $(date) '[info] cambridge_coffee_pot/run.sh completed'
+echo $(date) '[info] cambridge_coffee_pot/run.sh completed' >>/home/pi/cambridge_coffee_pot.log
 
